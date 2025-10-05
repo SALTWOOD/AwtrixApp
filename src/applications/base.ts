f3dc5f0e24a9583521b0cc88bb203af87f3a32d7
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2025-10-05 20:20:40 +0900

    add: base codes

---
+++ src/applications/base.ts
@@ -0,0 +1,14 @@
+import { Awtrix } from "../awtrix.js";
+
+export abstract class BaseApplication {
+    protected awtrix: Awtrix;
+    public interval: string; // crontab
+
+    constructor(awtrix: Awtrix, _data: any) {
+        this.awtrix = awtrix;
+        this.interval = _data.interval || '* * * * *';
+    }
+    abstract start(): Promise<void>;
+    abstract tick(): Promise<void>;
+    abstract stop(): Promise<void>;
+}
